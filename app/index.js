b978022f663c674dbe0262f0721c59e704ad213e
davemflick <davemflick@yahoo.com>
2017-05-03 11:03:42 -0400

    set-up basic react file structure, original content not yet incorporated

---
+++ app/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM, { react } from 'react-dom';
+
+class App extends React.Component {
+
+	render() {
+		return (
+			<div>
+				Yo
+			</div>
+		)
+	}
+
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
